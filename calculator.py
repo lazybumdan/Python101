91288ffd5996d29f8b6b99d04d0824edcdeec58e
Danielson <lazybumdan@gmail.com>
2026-04-21 14:07:24 +0400

    Refactor comments for clarity in calculator.py
    Updated comments to clarify the calculator's functionality.

--- calculator.py
+++ calculator.py
@@ -34,8 +34,8 @@ def calculator():
             
 calculator()
 
-The goal of the calculator is to allow users to do simple addition, subtraction, multiplication and division aswell as giving them the
-option to round the output number and also allowing them to either continue doing more calculations or stopping the code effectively 
-turning it off.
-# A little explaination on the code written above 
+# The goal of the calculator is to allow users to do simple addition, subtraction, multiplication and division aswell as giving them the
+# option to round the output number and also allowing them to either continue doing more calculations or stopping the code effectively 
+# turning it off.
+
 
