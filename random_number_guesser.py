4d7b08fc7157c0cb7c49e90fc7934db31b3d4f78
Danny <lazybumdan@gmail.com>
2026-05-04 14:53:41 +0400

    Added the ability for the code to restart if the user selects a number outside of 1 to 10

--- random_number_guesser.py
+++ random_number_guesser.py
@@ -2,13 +2,15 @@ import random
 
 def guess_the_number():
     while True:
-        number = int(input("Guess a number between 1 and 10: "))
-        guessed = random.randint(1, 10)
-        if number == guessed:
+        user_number = int(input("Guess a number between 1 and 10: "))
+        actual_number = random.randint(1, 10)
+        if user_number >= 10 or user_number < 1:
+            print("Please enter a number between 1 and 10")
+        elif user_number == actual_number:
             print("Congratulations! You guessed it!")
             break
         else:
-            retry = input(f"Sorry, that's wrong. The correct number is {guessed}. Would you like to try again?(Y/N)")
+            retry = input(f"Sorry, that's wrong. The correct number is {actual_number}. Would you like to try again?(Y/N)")
             if retry == "Y" or retry == "y":
                 continue
             else:
