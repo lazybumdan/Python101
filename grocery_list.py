eb9bff8534ce8749bcf6cc9ba79ebb0c80c17d43
Danny <lazybumdan@gmail.com>
2026-05-06 10:22:30 +0400

    Update grocery_list.py

--- grocery_list.py
+++ grocery_list.py
@@ -1,16 +1,17 @@
 def groceries():
     grocery_list = []
     while True:
-      add_items = str(input("What items do you need from the store?: "))
-      grocery_list.append(add_items)
-      finished_list = input("Are you done? Y/N")
-      if finished_list == "N":
-          continue
-      elif finished_list == "Y":
+      user_choice = int(input(
+          "What input would you like to do?\n 1. Add an item to the grocery list\n 2. Remove an item from the grocery list\n 3. Display the grocery list\n Enter you choice here:"))
+      if user_choice == 1:
+          grocery_item = input("Select an item:")
+          grocery_list.append(grocery_item)
+      elif user_choice == 2:
+          grocery_list.remove(grocery_item)
+      elif user_choice == 3:
           print(grocery_list)
           break
-      else:
-          continue
+
 
 
 groceries()
