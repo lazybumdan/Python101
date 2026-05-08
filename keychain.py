e7741d04eccdc9fd5ea19b3f649dfe522bde1ac9
Danny <lazybumdan@gmail.com>
2026-05-08 12:44:23 +0400

    Updated keychain.py

--- keychain.py
+++ keychain.py
@@ -1,21 +1,42 @@
-username = []
-password = []
-
+usernames = []
+passwords = []
+platforms = []
 def login():
     global username
     global password
-    user = input("Please enter your username: ")
-    passwords = input("Please enter your password: ")
-    username.append(user)
-    password.append(passwords)
+    global platforms
+
+def login_data():
+    while True:
+       user = input("Please enter your username: ")
+       password = input("Please enter your password: ")
+       platform = input("Please enter your platform: ")
+       usernames.append(user)
+       passwords.append(password)
+       decision = input("Would you like to continue? (y/n): ")
+       if decision == "y" or decision == "Y":
+           continue
+       elif decision == "n" or decision == "N":
+           break
+       else:
+           print("Sorry, I didn't understand that.")
+           continue
 
 def keychain():
-    global username
-    global password
-    print(f"Your usernames are {username} and the passwords are {password}")
+    global usernames
+    global passwords
+    global platforms
+    for user in usernames:
+        print(f"Your username is {user}")
+    for password in passwords:
+        print(f"Your password is {password}")
+    for platform in platforms:
+        print(f"Your platform is {platform}")
+
 
 def main():
     login()
+    login_data()
     keychain()
 
 main()
