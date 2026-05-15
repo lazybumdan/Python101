26b4e3482aed5729d2fcefd28d202209db95c1ae
Danny <lazybumdan@gmail.com>
2026-05-15 18:33:35 +0400

    Revamped the code for calculator.py
    Introduced more functions as well as the ability to only call on main if you're in the correct file, this allows me to import functions from this file into other files without calling on main.

--- calculator.py
+++ calculator.py
@@ -1,41 +1,41 @@
 # A very simple python calculator
 print("Welcome to Daniel's calculator! \n'It can add, subtract, multiply and divide any number!")
-def calculator():
-    while True:
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-        operator = input("Enter a operator, the options are +,-,*,/,%,**: ")
-        if operator == "+":
-            ans = (num1 + num2)
-            print(ans)
-        elif operator == "-":
-            ans = (num1 - num2)
-            print(ans)
-        elif operator == "*":
-            ans = (num1 * num2)
-            print(ans)
-        elif operator == "/":
-            ans = (num1 / num2)
-            print(ans)
-        elif operator == "%":
-            ans = (num1 % num2)
-            print(ans)
-        elif operator == "**":
-            ans = (num1 ** num2)
-            print(ans)
-        rounded = input("Do you want to round the calculation? (y/n): ")
-        if rounded == "y":
-            print(int(ans))
-        prompt = input("Would you like to continue? (y/n): ")
-        if prompt == "y":
-            continue
-        else:
-            break
-            
-calculator()
+x = float(input("What is your first number?: "))
+y = float(input("What is your second number?: "))
 
-# The goal of the calculator is to allow users to do simple addition, subtraction, multiplication and division aswell as giving them the
-# option to round the output number and also allowing them to either continue doing more calculations or stopping the code effectively 
-# turning it off.
+def addition():
+    print("The sum is: ", x + y)
+
+def subtraction():
+    print("The subtraction is: ", x - y)
+
+def multiply():
+    print("The multiplication is: ", x * y)
+
+def division():
+    print("The division is: ", x / y)
+
+def squaring():
+    print("The square is: ", x ** 2)
+
+def square_root():
+    print("The square root of is: ", x ** (1 / 2))
+
+def main():
+    choice = input(" 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division \n 5. Squaring \n 6. Square Root \n Please enter your choice 1-6:  ")
+    if choice == "1":
+        addition()
+    elif choice == "2":
+        subtraction()
+    elif choice == "3":
+        multiply()
+    elif choice == "4":
+        division()
+    elif choice == "5":
+        squaring()
+    elif choice == "6":
+        square_root()
 
 
+if __name__ == "__main__":
+    main()
