cea7ff64e190549d153258f502bb47635d935808
Danny <danielsonsanana9@gmail.com>
2026-05-30 15:12:15 +0400

    Created player.py

---
+++ gaming.py
@@ -0,0 +1,16 @@
+class Player:
+    def __init__(self, name, team, position, nationality):
+        
+    
+
+
+
+def player():
+    name = input("Player Name: ")
+    team = input("Team: ")
+    position = input("Position: ")
+    nationality = input("Nationality: ")
+    return Player(name, team, position, nationality)
+    
+def main():
+    player()
